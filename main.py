9cfe02c53297bf4be94778a55347c92426a9636c
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-09-06 09:13:49 +0600

    Homework-2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Hi! I'm Taseen. Nice to meet you.")
+
+me_birthday = 12.01
+print("My birthday is" ,me_birthday)
+mom_birthday = 22.09
+print("My mom's birthday is" ,mom_birthday)
+dad_birthday = 07.11
+print("My dad's birthday is" ,dad_birthday)
+grandpa_birthday = 04.12
+print("My  grandpa's birthday is" ,me_birthday)
+grandma_birthday = 11.02
+print("My grandma's birthday is" ,me_birthday)
